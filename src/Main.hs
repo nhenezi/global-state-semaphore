8bcd3f0230bee4dfe12e84fa47647189f28905c5
Nikola Henezi <nikola@henezi.com>
2016-09-03 20:08:55 +0200

    Refactoring Crossroad to records

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
-{-# Language OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 import System.Random
@@ -11,8 +12,9 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 type TrafficDensity = Double
 -- Crossroad can either allow horizontal pass, vertical pass or neither. If it's neither,
 -- then we mark that state as changing (e.g. it's changing from vertical to horizontal pass)
+type Location = String
 data CrossroadState = HPass | VPass | Changing deriving (Eq, Show, Read)
-data Crossroad = Crossroad CrossroadState deriving (Eq, Show, Read)
+data Crossroad = Crossroad { state ::CrossroadState, location :: Location} deriving (Eq, Show, Read)
 
 -- How long to pause between traffic light changesj
 changePause :: Double
@@ -22,6 +24,10 @@ changePause = 3.0
 minimumCrossroadState :: Double
 minimumCrossroadState = 5.0
 
+negateCState :: CrossroadState -> CrossroadState
+negateCState Changing = Changing
+negateCState HPass = VPass
+negateCState VPass = HPass
 
 -- value after which crossroad will consider changing state
 trafficDensityLimit :: TrafficDensity
@@ -29,29 +35,18 @@ trafficDensityLimit = 1.5
 
 -- default crossroad state
 initializeCrossroad :: Crossroad
-initializeCrossroad = Crossroad HPass
+initializeCrossroad = Crossroad HPass ""
 
 -- simulates traffic sensor by generating random value
 generateRandomDensity :: IO TrafficDensity
 generateRandomDensity = getStdRandom (randomR (0, 2))
 
--- hackish way to retrieve crossroad state
-getCrossroadState :: Crossroad -> CrossroadState
-getCrossroadState (Crossroad HPass) = HPass
-getCrossroadState (Crossroad VPass) = VPass
-getCrossroadState (Crossroad Changing) = Changing
-
 -- determines when a state of a crossroad has to be changed
 changeState :: Double -> TrafficDensity -> TrafficDensity -> Crossroad -> CrossroadState
-changeState _ _ _ (Crossroad Changing) = Changing
-changeState n nd1 nd2 (Crossroad HPass)
-  | n < minimumCrossroadState = HPass
-  | nd1 < nd2 = VPass
-  | otherwise = HPass
-changeState n nd1 nd2 (Crossroad VPass)
-  | n < minimumCrossroadState = VPass
-  | nd1 >= nd2 = HPass
-  | otherwise = VPass
+changeState n nd1 nd2 (Crossroad {..})
+  | n < minimumCrossroadState = state
+  | nd1 >= nd2 = negateCState state
+  | otherwise = state
 
 main :: IO ()
 main = do
@@ -67,18 +62,21 @@ main = do
     timeSinceLastChange <- readIORef timeSinceLastChangeIO -- get value since last change
     modifyIORef timeSinceLastChangeIO (const (timeSinceLastChange + 1)) -- update time since last change
     changing <- readIORef changingIO -- get changing value
+    c <- readIORef cIO -- get crossroad value
+    let ss = if changing == True then Crossroad Changing "" else c
+    MQ.publishMsg chan "SemaphoreExchange" "exchangeKey"
+      MQ.newMsg { MQ.msgBody = BL.pack $ show ss, MQ.msgDeliveryMode = Just MQ.Persistent}
     -- if crossroad is changing and time since last change is sufficient, then update crossroad
     -- to a new state, reset timer and set changing to False
     when (changing && timeSinceLastChange > changePause) $ do
       putStr "Changing! "
       changeTo <- readIORef changeToIO
-      modifyIORef cIO (const $ Crossroad changeTo)
+      modifyIORef cIO (const $ c { state = changeTo })
       modifyIORef timeSinceLastChangeIO (const 0.0)
       modifyIORef changingIO (const False)
     -- if crossroad is not changing state generate random densities and check if state
     -- has to be changed
-    c <- readIORef cIO -- get crossroad value
-    let currentState = getCrossroadState c
+    let currentState = state c
     unless changing $ do
       putStr "Not changing! "
       newD1 <- generateRandomDensity
@@ -93,9 +91,5 @@ main = do
         modifyIORef changeToIO (const newState)
         modifyIORef timeSinceLastChangeIO (const 0.0)
     -- send state information to SemaphoreExchange exchange on RabbitMQ
-    changingTmp <- readIORef changingIO
-    let ss = if changingTmp == True then Crossroad Changing else c
-    MQ.publishMsg chan "SemaphoreExchange" "exchangeKey"
-      MQ.newMsg { MQ.msgBody = BL.pack $ show ss, MQ.msgDeliveryMode = Just MQ.Persistent}
     threadDelay 1000000 -- throttle everything so above happens every ~1 sec
 
