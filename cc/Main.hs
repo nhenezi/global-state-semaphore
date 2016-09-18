33cc520e8ccdcf9d754c35aa830cc440daf6b7b2
Nikola Henezi <nikola@henezi.com>
2016-09-18 21:12:24 +0200

    Generating global state every 5 seconds

--- cc/Main.hs
+++ cc/Main.hs
@@ -1,8 +1,9 @@
 {-# Language OverloadedStrings #-}
 module Main where
 
+import Prelude
 import Control.Monad.IO.Class (liftIO)
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay, forkIO)
 import Control.Monad (forever)
 import Control.Monad.State
 import Data.IORef
@@ -21,14 +22,27 @@ main = do
                    { MQ.queueName = "semaphoreStatus" }
   MQ.bindQueue chan queue "SemaphoreExchange" "exchangeKey"
   stateIO <- newIORef ([] :: [Crossroad]) -- global state of the system
+  canEnterThreadIO <- newIORef True -- global state of the system
+  timeSinceLastPrintIO <- newIORef 0 -- when did we last print the output?
   forever $ do
+    running <- readIORef canEnterThreadIO
+    when running $ void $ forkIO (do
+                      modifyIORef canEnterThreadIO (const False)
+                      t <- readIORef timeSinceLastPrintIO
+                      modifyIORef timeSinceLastPrintIO (const (t + 1))
+                      threadDelay 1000000 -- throttle ~1 sec
+                      modifyIORef canEnterThreadIO (const True)
+                      )
     s <- readIORef stateIO
     mMsg <- liftIO $ MQ.getMsg chan MQ.Ack queue
     case mMsg of
       Nothing  -> liftIO $ threadDelay (10 * 100000) -- if there are no messages throttle ~100 msec
       Just envelope -> do
         let newS = execState (updateStatus chan envelope) s
-        putStrLn $ pprint (GlobalState newS)
+        t <- readIORef timeSinceLastPrintIO
+        if t > 5 then (do
+                      modifyIORef timeSinceLastPrintIO (const 0)
+                      putStrLn (pprint (GlobalState newS))) else return ()
         modifyIORef stateIO (const newS)
         liftIO $ MQ.ackEnv (snd envelope)
         return ()
